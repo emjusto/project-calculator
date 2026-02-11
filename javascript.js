5ed3085f8673d19d0204f96474389ddcb7992155
EM <119482708+emjusto@users.noreply.github.com>
2026-02-11 08:03:09 +0800

    Added num array

--- javascript.js
+++ javascript.js
@@ -1,5 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
-    
+    let calculatorCon = document.querySelector('.calc-box');
+    let resultCon = document.querySelector('.result');
+    let numCon = document.querySelector('.numbers');
+
+    let numArray = [
+        { id: "clearAll",text: "AC" },
+        {id: "delete", text: "del" },
+        { id: "divide", text: "/" },
+        { id: "7", text: "7" },
+        { id: "8", text: "8" },
+        { id: "9", text: "9" },
+        { id: "multiply", text: "x" },
+        { id: "4", text: "4" },
+        { id: "5", text: "5" },
+        { id: "6", text: "6" },
+        { id: "subtract", text: "-" },
+        { id: "1", text: "1" },
+        { id: "2", text: "2" },
+        { id: "3", text: "3" },
+        { id: "add", text: "+" },
+        { id: "0", text: "0" },
+        { id: "decimal", text: "." },
+        { id: "equals", text: "=" },]
+
+    for (let i=0; i<=19; 1++) {
+        let numBox = document.createElement('div');
+        numBox
+    }
 
 
 
